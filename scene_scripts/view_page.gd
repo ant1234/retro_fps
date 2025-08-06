63a675ba37bff2af01ec3868671d14e97aa4b9c7
ant1234 <antonyvidetta1986@gmail.com>
2025-08-06 20:08:07 +1200

    Scroll through photos of the same subject on the view page

--- scene_scripts/view_page.gd
+++ scene_scripts/view_page.gd
@@ -5,14 +5,23 @@ extends Control
 @onready var image_description: RichTextLabel = $RightBackground/BannerBackground/ImageDescription
 @onready var image_container: HBoxContainer = $RightBackground/BannerBackground/ImageContainer
 @onready var badge_icon: TextureRect = $RightBackground/BannerBackground/BadgeIcon
+@onready var scroll_left: Button = $RightBackground/ScrollLeft
+@onready var scroll_right: Button = $RightBackground/ScrollRight
 
 const META_DIR := "user://photo_json"
 
+var subject_photos: Array = []
+var current_photo_index: int = 0
+
 func _ready():
+	_load_all_photos_for_subject()
 	_load_selected_photo()
 	_show_prompt_dialogue()
+	
 	to_album_page.pressed.connect(_on_to_album_page_pressed)
 	mark_page.pressed.connect(_on_mark_page_pressed)
+	scroll_left.pressed.connect(_on_scroll_left_pressed)
+	scroll_right.pressed.connect(_on_scroll_right_pressed)
 
 func _on_mark_page_pressed():
 	_mark_photo_and_clear_others()
@@ -129,3 +138,62 @@ func _show_prompt_dialogue():
 
 func _on_to_album_page_pressed():
 	SceneRouter.goto_scene("res://scenes/album_page.tscn")
+	
+func _load_all_photos_for_subject():
+	var selected_meta = GameState.selected_photo_meta
+	var subject_name = selected_meta.get("subject_name", "")
+	if subject_name == "":
+		printerr("No subject name found in selected meta.")
+		return
+
+	var dir = DirAccess.open(META_DIR)
+	if not dir:
+		printerr("Failed to open metadata directory.")
+		return
+
+	subject_photos.clear()
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with(".json"):
+			var path = META_DIR + "/" + file_name
+			var file = FileAccess.open(path, FileAccess.READ)
+			if file:
+				var data = JSON.parse_string(file.get_as_text())
+				file.close()
+				if typeof(data) == TYPE_DICTIONARY and data.get("subject_name", "") == subject_name:
+					var image_path = "user://photos/" + file_name.replace(".json", ".png")
+					if FileAccess.file_exists(image_path):
+						subject_photos.append({
+							"meta": data,
+							"path": image_path
+						})
+		file_name = dir.get_next()
+	dir.list_dir_end()
+
+	# Sort for consistency
+	subject_photos.sort_custom(func(a, b): return a["path"] < b["path"])
+
+	# Find index of currently selected photo
+	current_photo_index = subject_photos.find(subject_photos.filter(func(p): return p["path"] == GameState.selected_photo_path).front())
+	
+
+func _on_scroll_left_pressed():
+	if subject_photos.size() == 0:
+		return
+
+	current_photo_index = (current_photo_index - 1 + subject_photos.size()) % subject_photos.size()
+	_load_photo_by_index()
+
+func _on_scroll_right_pressed():
+	if subject_photos.size() == 0:
+		return
+
+	current_photo_index = (current_photo_index + 1) % subject_photos.size()
+	_load_photo_by_index()
+
+func _load_photo_by_index():
+	var entry = subject_photos[current_photo_index]
+	GameState.selected_photo_path = entry["path"]
+	GameState.selected_photo_meta = entry["meta"]
+	_load_selected_photo()
