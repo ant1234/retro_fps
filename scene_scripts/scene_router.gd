4d349ef7013c59769b232fa88e9c06a0397e71a3
ant1234 <antonyvidetta1986@gmail.com>
2025-08-08 22:04:05 +1200

    Fix bug in router when transition 3D scenes into 2D

--- scene_scripts/scene_router.gd
+++ scene_scripts/scene_router.gd
@@ -1,14 +1,23 @@
 extends Node
 
-func _ready():
-	call_deferred("goto_scene", "res://world.tscn")
+var _current_scene_path: String = ""
 
 func goto_scene(scene_path: String) -> void:
+	if scene_path == _current_scene_path:
+		print("Already in this scene, skipping:", scene_path)
+		return
+
+	_current_scene_path = scene_path
+
 	var scene_resource = load(scene_path)
 	if scene_resource:
+		print("Scene loaded: ", scene_path)
 		var tree = Engine.get_main_loop()
 		if tree and tree is SceneTree:
-			tree.change_scene_to_packed(scene_resource)
+			print("Scene tree is valid. Changing scene...")
+			var err = tree.change_scene_to_packed(scene_resource)
+			if err != OK:
+				printerr("Scene change failed with error code: ", err)
 		else:
 			printerr("SceneTree not found.")
 	else:
