5d4c3217c3fb68f86db543449b54ecabe4a68bb3
ant1234 <antonyvidetta1986@gmail.com>
2025-08-14 16:21:00 +1200

    Fix the movement direction in sub

--- characters/player/step_tracker.gd
+++ characters/player/step_tracker.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @onready var character_mover : CharacterMover = get_parent()
-@onready var step_sounds: Node3D = $StepSounds
 
 @export var step_after_distance = 3.0
 @onready var last_pos = global_position
@@ -14,6 +13,5 @@ func _physics_process(delta: float) -> void:
 	distance_travelled_since_last_step += global_position.distance_to(last_pos)
 	if distance_travelled_since_last_step >= step_after_distance:
 		distance_travelled_since_last_step = 0.0
-		step_sounds.play()
 		
 	last_pos = global_position
